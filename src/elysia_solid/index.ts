df56d92eb413eebb6872ad144a4deb7d2d0848dc
paletteOvO <paletteOvO@outlook.com>
2024-04-26 16:58:58 +0800

    no longer build server componet on runtime for better type hinting

--- src/elysia_solid/index.ts
+++ src/elysia_solid/index.ts
@@ -11,55 +11,55 @@ const _hydrations = new Map<string, Promise<string>>();
 const _components = new Map<string, (props: unknown) => JSXElement>();
 
 const buildComponent = async (
-	componentPath: string,
+  componentPath: string,
 ): Promise<(props: unknown) => JSXElement> => {
-	const path = (
-		(await build(
-			buildConfig({
-				entryScript: `export { default as App } from "${componentPath}"`,
-				format: "esm",
-				ssr: true,
-			}),
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		)) as any
-	).output[0].fileName;
-	return (await import(`./${path}`)).App;
+  const path = (
+    (await build(
+      buildConfig({
+        entryScript: `export { default as App } from "${componentPath}"`,
+        format: "esm",
+        ssr: true,
+      }),
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    )) as any
+  ).output[0].fileName;
+  return (await import(`./${path}`)).App;
 };
 
 export default new Elysia()
-	.decorate("renderPage", async (componentPath: string, props: unknown) => {
-		const md5 = crypto.createHash("md5");
-		const hash = md5.update(componentPath).digest("hex");
+  .decorate("renderPage", async<P>(
+    component: Promise<{ "default": (props: P) => JSXElement }>,
+    componentPath: string, props: P) => {
+    const md5 = crypto.createHash("md5");
+    const hash = md5.update(componentPath).digest("hex");
 
-		// cache the server side component
-		if (!_components.has(hash)) {
-			_components.set(hash, await buildComponent(componentPath));
-		}
+    // cache the server side component
+    if (!_components.has(hash)) {
+      _components.set(hash, await buildComponent(componentPath));
+    }
 
-		const component = _components.get(hash) as (props: unknown) => JSXElement;
+    // cache the client side hydrate script for /_hydrate.js
+    if (!_hydrations.has(hash)) {
+      _hydrations.set(hash, hydrateScript(componentPath));
+    }
 
-		// cache the client side hydrate script for /_hydrate.js
-		if (!_hydrations.has(hash)) {
-			_hydrations.set(hash, hydrateScript(componentPath));
-		}
-
-		return await renderPage(component, props, hash);
-	})
-	.get(
-		"/_hydrate.js",
-		async ({ query: { hash } }) => {
-			const hydrationScript = _hydrations.get(hash);
-			if (!hydrationScript) {
-				throw new NotFoundError();
-			}
-			return await Bun.file(await hydrationScript).text();
-		},
-		{
-			query: t.Object({
-				hash: t.String(),
-			}),
-			afterHandle: async ({ set }) => {
-				set.headers["content-type"] = "application/javascript; charset=utf8";
-			},
-		},
-	);
+    return await renderPage((await component).default, props, hash);
+  })
+  .get(
+    "/_hydrate.js",
+    async ({ query: { hash } }) => {
+      const hydrationScript = _hydrations.get(hash);
+      if (!hydrationScript) {
+        throw new NotFoundError();
+      }
+      return await Bun.file(await hydrationScript).text();
+    },
+    {
+      query: t.Object({
+        hash: t.String(),
+      }),
+      afterHandle: async ({ set }) => {
+        set.headers["content-type"] = "application/javascript; charset=utf8";
+      },
+    },
+  );
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,24 @@ import { Elysia } from "elysia";
 import elysiaSolid from "./elysia_solid";
 
 const app = new Elysia()
-	.use(elysiaSolid)
-	.get(
-		"/",
-		async ({ renderPage }) => {
-			return await renderPage("@src/pages/index", {
-				counter: 42,
-			} as Parameters<typeof import("@src/pages/index")["default"]>[0])
-		},
-		{
-			afterHandle: async ({ set }) => {
-				set.headers["content-type"] = "text/html; charset=utf8";
-			},
-		},
-	)
-	.listen(3000);
+  .use(elysiaSolid)
+  .get(
+    "/",
+    async ({ renderPage }) => {
+      return await renderPage(
+        import("@src/pages/index"),
+        "@src/pages/index", {
+        counter: 42,
+      })
+    },
+    {
+      afterHandle: async ({ set }) => {
+        set.headers["content-type"] = "text/html; charset=utf8";
+      },
+    },
+  )
+  .listen(3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
